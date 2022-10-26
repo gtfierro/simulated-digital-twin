c77eff525acafb136e2bfa769ed8ca8d70b4771b
Gabe Fierro <gtfierro@mines.edu>
2022-10-26 11:27:22 -0600

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+git submodule init
+git submodule update --init --recursive
+
+cd boptest-bacnet-proxy
+git apply ../boptest-proxy.patch
+cp ../Dockerfile.boptest-proxy Dockerfile
+cp ../run.sh .
+
+cd project1-boptest
+git apply ../boptest.patch
+
+make build
+
+make up
