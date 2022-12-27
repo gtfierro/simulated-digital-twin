8790cd6ebd3aae9f50f7383e8f36c7fb2b39c78e
Gabe Fierro <gtfierro@mines.edu>
2022-12-27 16:08:40 -0700

    url typo

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 while true ; do
     sleep 10 
-    python BopTestProxy.py simple.ttl 19740 3600 --baseurl http//boptest:5000
+    python BopTestProxy.py simple.ttl 19740 3600 --baseurl http://boptest:5000
 done
